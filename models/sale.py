1d9f5709a2bb275f40f0522abdd8c5d13d6b0629
Valentin Magaña <valentin.magana@adweb.mx>
2017-07-14 12:41:59 -0500

    Versión funcional
    -Se realizaron modificaciones en el modelo de sale.py y en la vista
    sale_view.xml
    -Se añadió el campo ae_renta_dias para utilizar como global

--- models/sale.py
+++ models/sale.py
@@ -5,54 +5,46 @@ import odoo.addons.decimal_precision as dp
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    ae_renta = fields.Boolean(string="Renta", help="")
+    ae_renta_dias = fields.Integer()
+
+    ae_renta_dias = 1
+
     @api.depends('order_line.price_total')
     def _amount_all(self):
-        """
-        Compute the total amounts of the SO.
-        """
         for order in self:
             amount_untaxed = amount_tax = amount_discount = 0.0
             for line in order.order_line:
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
-                amount_discount += (line.product_uom_qty * line.price_unit * line.discount)/100
+                amount_discount += line.product_uom_qty * line.price_unit * ae_renta_dias
             order.update({
                 'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
                 'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
-                'amount_discount': order.pricelist_id.currency_id.round(amount_discount),
-                'amount_total': amount_untaxed + amount_tax,
+                'amount_discount': ae_renta_dias,
+                'amount_total': (amount_untaxed + amount_tax) * ae_renta_dias,
             })
 
     discount_type = fields.Selection([('percent', 'Percentage'), ('amount', 'Amount')], string='Discount type',
                                      readonly=True,states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
                                      default='percent')
-    discount_rate = fields.Float('Discount Rate', digits_compute=dp.get_precision('Account'),
-                                 readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+    discount_rate = fields.Integer('Dias de renta', digits_compute=dp.get_precision('Account'),
+                                 readonly=False, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
     amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
                                      track_visibility='always')
     amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
                                  track_visibility='always')
     amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
                                    track_visibility='always')
-    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True, compute='_amount_all',
+    amount_discount = fields.Integer(string='Discount', store=True, readonly=True, compute='_amount_all',
                                       digits_compute=dp.get_precision('Account'), track_visibility='always')
 
     @api.onchange('discount_type', 'discount_rate', 'order_line')
     def supply_rate(self):
+        global ae_renta_dias
         for order in self:
-            if order.discount_type == 'percent':
-                for line in order.order_line:
-                    line.discount = order.discount_rate
-            else:
-                total = discount = 0.0
-                for line in order.order_line:
-                    total += round((line.product_uom_qty * line.price_unit))
-                if order.discount_rate != 0:
-                    discount = (order.discount_rate / total) * 100
-                else:
-                    discount = order.discount_rate
-                for line in order.order_line:
-                    line.discount = discount
+            for line in order.order_line:
+                ae_renta_dias = line.discount = order.discount_rate
 
     @api.multi
     def _prepare_invoice(self,):
@@ -114,9 +106,9 @@ class AccountTax(models.Model):
                 base -= tax_amount
             else:
                 total_included += tax_amount
-            
+
             tax_base = base
-            
+
             if tax.include_base_amount:
                 base += tax_amount
 
@@ -145,4 +137,3 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     discount = fields.Float(string='Discount (%)', digits=(16, 20), default=0.0)
-
